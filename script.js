4b565790bc59be0641d6b949be680455cf732de2
Mayara Mayara <mayarabraineraguiar@gmail.com>
2024-03-21 00:05:23 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+window.onload = function ()
+{
+    var header = document.getElementById('header');
+    var footer = document.getElementById('footer');
+
+    header.innerHTML = "<nav><span class='logo'>BrainerCoffee</span><ul class='menu'><li><a>Home</a></li><li><a>Receitas</a></li><li><a>Novidades</a></li><li><a>Sobre Nós</a></li> </ul><a class='GitHub'>GitHub</a></nav>";
+    footer.innerHTML = "<p>Copyright &lt;CODEFEE /&gt; 2023</p> <br> <p>Feito com muito amor e café por Mayara Brainer</p>";
+}
